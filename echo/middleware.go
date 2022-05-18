0f5398ebaf53af175fc6ace88099a2ea2d165bdf
asafc <asafc64@gmail.com>
2022-05-18 15:41:08 +0300

    Added middleware for Echo

--- echo/middleware.go
+++ echo/middleware.go
@@ -1,18 +1,19 @@
 package echo
 
 import (
-	"fmt"
-
 	"github.com/labstack/echo/v4"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
 )
 
-func Middleware(e *echo.Echo) echo.MiddlewareFunc {
+func Middleware() echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
-			//key := c.Request().Method+c.Request().RequestURI
-			for _, r := range e.Routes() {
+			for _, r := range c.Echo().Routes() {
 				if r.Method == c.Request().Method && r.Path == c.Path() {
-					fmt.Println(r.Name)
+					span := trace.SpanFromContext(c.Request().Context())
+					span.SetAttributes(attribute.String("endpoint.function_full_name", r.Name))
+					break
 				}
 			}
 			return next(c)
