b61cad4c504961589de4f004fa96bc01f857eea7
arik-dig <asher@digma.ai>
2022-06-15 10:16:06 +0300

    add comments, remove un needed client interceptors

--- grpc/grpc-interceptor.go
+++ grpc/grpc-interceptor.go
@@ -11,51 +11,32 @@ import (
 
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/trace"
-	//
-	//"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
 )
 
-// UnaryClientInterceptor returns a grpc.UnaryClientInterceptor suitable
-// for use in a grpc.Dial call.
-func UnaryClientInterceptor() grpc.UnaryClientInterceptor {
-	return func(
-		ctx context.Context,
-		method string,
-		req, reply interface{},
-		cc *grpc.ClientConn,
-		invoker grpc.UnaryInvoker,
-		callOpts ...grpc.CallOption,
-	) error {
+//
+// Digma's grpc-interceptor:
+// should be chained after the original otelgrpc interceptors.
+//
+// usage for example :
+//  import (
+//     	"google.golang.org/grpc"
+//      "go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
+//  	digma_grpc "github.com/digma-ai/otel-go-instrumentation/grpc"
+//   	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
+//  )
+//
+//	server := grpc.NewServer(
+//	    grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(
+//	       otelgrpc.UnaryServerInterceptor(),
+//	       digmagrpc.UnaryServerInterceptor(),
+//      )),
+//      grpc.StreamInterceptor(grpc_middleware.ChainStreamServer(
+//	      otelgrpc.StreamServerInterceptor(),
+//	      digma_grpc.StreamServerInterceptor(),
+//      )),
+//  )
+//
 
-		span := trace.SpanFromContext(ctx)
-		span.SetAttributes(attribute.String("endpoint.function_full_name", method))
-
-		// standard call of interceptor
-		err := invoker(ctx, method, req, reply, cc, callOpts...)
-		return err
-	}
-}
-
-// StreamClientInterceptor returns a grpc.StreamClientInterceptor suitable
-// for use in a grpc.Dial call.
-func StreamClientInterceptor() grpc.StreamClientInterceptor {
-	return func(
-		ctx context.Context,
-		desc *grpc.StreamDesc,
-		cc *grpc.ClientConn,
-		method string,
-		streamer grpc.Streamer,
-		callOpts ...grpc.CallOption,
-	) (grpc.ClientStream, error) {
-
-		span := trace.SpanFromContext(ctx)
-		span.SetAttributes(attribute.String("endpoint.function_full_name", method))
-
-		// standard call of interceptor
-		clientStream, err := streamer(ctx, desc, cc, method, callOpts...)
-		return clientStream, err
-	}
-}
 
 // UnaryServerInterceptor returns a grpc.UnaryServerInterceptor suitable
 // for use in a grpc.NewServer call.
