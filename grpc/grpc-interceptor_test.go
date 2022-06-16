60446968a18f4cc53c0513a11a8cc066cd4d3c83
arik-dig <asher@digma.ai>
2022-06-16 17:48:00 +0300

    add TestBuildMethodFqnGoodPath

--- grpc/grpc-interceptor_test.go
+++ grpc/grpc-interceptor_test.go
@@ -1,12 +1,37 @@
 package grpc
 
 import (
-    "testing"
+	"strings"
+	"testing"
+
 	"github.com/stretchr/testify/assert"
 )
 
 func TestMethodOnly(t *testing.T) {
 
-	assert.Equal(t, "xyz", methodOnly("/abcd/xyz"))
+	assert.Equal(t, "Xyz", methodOnly("/Abcd/Xyz"))
+}
 
+func TestBuildMethodFqnGoodPath(t *testing.T) {
+
+	impl := &impl4tests{}
+	methodFqn, err := buildMethodFqn(impl, "/api/DoSomething")
+	assert.Nil(t, err, "err should be nil")
+	assertEndsWith(t, methodFqn, "(*impl4tests).DoSomething")
+}
+
+func assertEndsWith(t *testing.T, entireValue string, expectedSuffix string) {
+	assert.True(t, strings.HasSuffix(entireValue, expectedSuffix), "'"+entireValue+"' has no expected suffix '"+expectedSuffix+"'")
+}
+
+type iface4tests interface {
+	DoSomething()
+}
+
+type impl4tests struct {
+	iface4tests
+}
+
+func (impl *impl4tests) DoSomething() {
+	// nothing, just signature
 }
